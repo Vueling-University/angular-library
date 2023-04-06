63c81987095b128b38af3e5d8a8547cea2dc14ff
sergiJimenez <acasualcolumbus@gmail.com>
2023-04-06 13:00:59 +0200

    Removed header closes #9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { HeaderModule, FooterModule, BreadcrumbModule } from 'projects/ui-workbench/src/public-api';
-import { TranslatingComponent } from './translating/translating.component';
+import {
+  FooterModule,
+  BreadcrumbModule,
+} from 'projects/ui-workbench/src/public-api';
 
 @NgModule({
-  declarations: [AppComponent, TranslatingComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HeaderModule,
-    FooterModule,
-    BreadcrumbModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FooterModule, BreadcrumbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
