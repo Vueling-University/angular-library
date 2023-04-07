c3a54592efe71c80eda78efc067e41cc271399bb
sergiJimenez <acasualcolumbus@gmail.com>
2023-04-07 11:23:50 +0200

    Deleted breadcrumb closes #13

--- projects/ui-workbench/src/public-api.ts
+++ projects/ui-workbench/src/public-api.ts
@@ -7,7 +7,5 @@ export * from './lib/ui-workbench.component';
 export * from './lib/ui-workbench.module';
 export * from './modules/footer/footer.module';
 export * from './modules/footer/footer-component/footer-component.component';
-export * from './modules/breadcrumb/breadcrumb.module';
-export * from './modules/breadcrumb/breadcrumb/breadcrumb.component';
 export * from './modules/error-view/error-view.module';
 export * from './modules/error-view/error-view/error-view.component';
