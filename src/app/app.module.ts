e52c5aaedbac7c63a0cc13acb9db18be1c727fde
sergiJimenez <acasualcolumbus@gmail.com>
2023-04-11 12:39:27 +0200

    Finishing last styles closes #15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import {
   FooterModule,
-  BreadcrumbModule,
   ErrorViewModule,
 } from 'projects/ui-workbench/src/public-api';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FooterModule,
-    BreadcrumbModule,
-    ErrorViewModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, FooterModule, ErrorViewModule],
   providers: [],
   bootstrap: [AppComponent],
 })
